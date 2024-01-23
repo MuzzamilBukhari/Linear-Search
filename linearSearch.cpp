b3b2c02c04111de4aadd15a345001e9743eec8da
MuzzamilBukhari <muhammadmuzzamilbukhari@gmail.com>
2024-01-23 17:33:22 +0500

    make a function

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 using namespace std;
+// Function to Search the number using linear search
+int linearSearch (int arr[], int size, int toSearch){
+   int i = 0;
+   for (i; i<size;i++){
+      if (arr[i] == toSearch){
+         return i;
+      }
+   }
+   if (i == size){
+      return -1;
+   }
+}
 int main (){
-     int arr[8] = {23, 14, 45, 32, 77, 49, 12, 27};
-     int n = sizeof(arr) / sizeof(int);
-     int toSearch = 3;
-     int i = 0;
-     for (i; i<n;i++){
-        if (arr[i] == toSearch){
-            cout<<"Found";
-            break;
-        }
-     }
-     if (i == n){
-        cout<<"Not Found";
-     }
+   int size;
+   cout<<"Enter the size of an array : ";
+   cin >> size;
+   int arr[size];
+   int toSearch;
+   cout<< "Enter array elements"<<endl;
+   for (int i = 0;i<size;i++){
+      cin >> arr[i];
+   }
+   cout<<"Enter the number you want to search in this array : ";
+   cin >> toSearch;
+   int pos = linearSearch(arr, size, toSearch);
+   pos >= 0 ? cout<<"Found at position "<<pos : cout<<"Not found"; 
 }
